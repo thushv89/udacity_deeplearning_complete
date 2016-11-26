a45685930a51a046030b0752c603a1b00e00294f
Local Admin <Local Admin>
2016-11-26 11:21:34 +1100

    continuing assignment 6 lstm with embeddings

--- 6_lstm_bigrams.py
+++ 6_lstm_bigrams.py
@@ -37,117 +37,6 @@ def read_data(filename):
 text = read_data(filename)
 print('Data size %d' % len(text))
 
-valid_size = 1000
-valid_text = text[:valid_size]
-train_text = text[valid_size:]
-train_size = len(train_text)
-print(train_size, train_text[:64])
-print(valid_size, valid_text[:64])
-
-vocabulary_size = len(string.ascii_lowercase) + 1 # [a-z] + ' '
-first_letter = ord(string.ascii_lowercase[0])
-
-# map character to an id
-# e.g. ' '=>0, a=> 1, b=>2, ..., z=>26
-def char2id(char):
-  if char in string.ascii_lowercase:
-    return ord(char) - first_letter + 1
-  elif char == ' ':
-    return 0
-  else:
-    print('Unexpected character: %s' % char.encode('utf-8'))
-    return 0
-
-def id2char(dictid):
-  if dictid > 0:
-    return chr(dictid + first_letter - 1)
-  else:
-    return ' '
-
-print(char2id('a'), char2id('z'), char2id(' '), char2id('ï'))
-print(id2char(1), id2char(26), id2char(0))
-
-
-batch_size=64
-num_unrollings=10 # new number of batches to add to training data
-
-class BatchGenerator(object):
-  def __init__(self, text, batch_size, num_unrollings):
-    self._text = text
-    self._text_size = len(text)
-    self._batch_size = batch_size
-    self._num_unrollings = num_unrollings
-    segment = self._text_size // batch_size
-    self._cursor = [ offset * segment for offset in range(batch_size)]
-    self._last_batch = self._next_batch()
-
-  def _next_batch(self):
-    """Generate a single batch from the current cursor position in the data."""
-    batch = np.zeros(shape=(self._batch_size, vocabulary_size), dtype=np.float)
-    for b in range(self._batch_size):
-      batch[b, char2id(self._text[self._cursor[b]])] = 1.0
-      self._cursor[b] = (self._cursor[b] + 1) % self._text_size
-    return batch
-
-  def next(self):
-    """Generate the next array of batches from the data. The array consists of
-    the last batch of the previous array, followed by num_unrollings new ones.
-    """
-    batches = [self._last_batch]
-    for step in range(self._num_unrollings):
-      batches.append(self._next_batch())
-    self._last_batch = batches[-1]
-    return batches
-
-def characters(probabilities):
-  """Turn a 1-hot encoding or a probability distribution over the possible
-  characters back into its (most likely) character representation."""
-  return [id2char(c) for c in np.argmax(probabilities, 1)]
-
-def batches2string(batches):
-  """Convert a sequence of batches back into their (most likely) string
-  representation."""
-  s = [''] * batches[0].shape[0]
-  for b in batches:
-    s = [''.join(x) for x in zip(s, characters(b))]
-  return s
-
-train_batches = BatchGenerator(train_text, batch_size, num_unrollings)
-valid_batches = BatchGenerator(valid_text, 1, 1)
-
-print(batches2string(train_batches.next()))
-print(batches2string(train_batches.next()))
-print(batches2string(valid_batches.next()))
-print(batches2string(valid_batches.next()))
-
-
-def logprob(predictions, labels):
-  """Log-probability of the true labels in a predicted batch."""
-  predictions[predictions < 1e-10] = 1e-10
-  return np.sum(np.multiply(labels, -np.log(predictions))) / labels.shape[0]
-
-def sample_distribution(distribution):
-  """Sample one element from a distribution assumed to be an array of normalized
-  probabilities.
-  """
-  r = random.uniform(0, 1)
-  s = 0
-  for i in range(len(distribution)):
-    s += distribution[i]
-    if s >= r:
-      return i
-  return len(distribution) - 1
-
-def sample(prediction):
-  """Turn a (column) prediction into 1-hot encoded samples."""
-  p = np.zeros(shape=[1, vocabulary_size], dtype=np.float)
-  p[0, sample_distribution(prediction[0])] = 1.0
-  return p
-
-def random_distribution():
-  """Generate a random column of probabilities."""
-  b = np.random.uniform(0.0, 1.0, size=[1, vocabulary_size])
-  return b/np.sum(b, 1)[:,None]
 
 def read_data_for_embed(filename):
     """Extract the first file enclosed in a zip file as a list of words"""
@@ -252,6 +141,97 @@ def generate_batch(batch_size, skip_window):
     assert batch.shape[0]==batch_size and batch.shape[1]== span-1
     return batch, labels
 
+valid_size = 1000
+valid_text = text[:valid_size]
+train_text = text[valid_size:]
+train_size = len(train_text)
+print(train_size, train_text[:64])
+print(valid_size, valid_text[:64])
+
+vocabulary_size = len(count)
+first_letter = ord(string.ascii_lowercase[0])
+
+
+batch_size=64
+num_unrollings=10 # new number of batches to add to training data
+
+class BatchGenerator(object):
+  def __init__(self, text_as_bigrams, batch_size, num_unrollings):
+    self._text = text_as_bigrams
+    self._text_size = len(text)
+    self._batch_size = batch_size
+    self._num_unrollings = num_unrollings
+    segment = self._text_size // batch_size
+    self._cursor = [ offset * segment for offset in range(batch_size)]
+    self._last_batch = self._next_batch()
+
+  def _next_batch(self):
+    """Generate a single batch from the current cursor position in the data."""
+    batch = np.zeros(shape=(self._batch_size, vocabulary_size), dtype=np.float)
+    for b in range(self._batch_size):
+      batch[b, dictionary(self._text[self._cursor[b]])] = 1.0
+      self._cursor[b] = (self._cursor[b] + 1) % self._text_size
+    return batch
+
+  def next(self):
+    """Generate the next array of batches from the data. The array consists of
+    the last batch of the previous array, followed by num_unrollings new ones.
+    """
+    batches = [self._last_batch]
+    for step in range(self._num_unrollings):
+      batches.append(self._next_batch())
+    self._last_batch = batches[-1]
+    return batches
+
+def characters(probabilities):
+  """Turn a 1-hot encoding or a probability distribution over the possible
+  characters back into its (most likely) character representation."""
+  return [id2char(c) for c in np.argmax(probabilities, 1)]
+
+def batches2string(batches):
+  """Convert a sequence of batches back into their (most likely) string
+  representation."""
+  s = [''] * batches[0].shape[0]
+  for b in batches:
+    s = [''.join(x) for x in zip(s, characters(b))]
+  return s
+
+train_batches = BatchGenerator(train_text, batch_size, num_unrollings)
+valid_batches = BatchGenerator(valid_text, 1, 1)
+
+print(batches2string(train_batches.next()))
+print(batches2string(train_batches.next()))
+print(batches2string(valid_batches.next()))
+print(batches2string(valid_batches.next()))
+
+
+def logprob(predictions, labels):
+  """Log-probability of the true labels in a predicted batch."""
+  predictions[predictions < 1e-10] = 1e-10
+  return np.sum(np.multiply(labels, -np.log(predictions))) / labels.shape[0]
+
+def sample_distribution(distribution):
+  """Sample one element from a distribution assumed to be an array of normalized
+  probabilities.
+  """
+  r = random.uniform(0, 1)
+  s = 0
+  for i in range(len(distribution)):
+    s += distribution[i]
+    if s >= r:
+      return i
+  return len(distribution) - 1
+
+def sample(prediction):
+  """Turn a (column) prediction into 1-hot encoded samples."""
+  p = np.zeros(shape=[1, vocabulary_size], dtype=np.float)
+  p[0, sample_distribution(prediction[0])] = 1.0
+  return p
+
+def random_distribution():
+  """Generate a random column of probabilities."""
+  b = np.random.uniform(0.0, 1.0, size=[1, vocabulary_size])
+  return b/np.sum(b, 1)[:,None]
 
 num_nodes = 64
 skip_window = 2
